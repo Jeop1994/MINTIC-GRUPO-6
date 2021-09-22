2307aff629196ddf1a71d722b74f15eedddd9577
Jeop1994 <jeordonezp@hotmail.com>
2021-09-22 18:42:45 -0500

    inicio con js

---
+++ form.js
@@ -0,0 +1,22 @@
+var nombreUsuario = document.getElementsById("campoUsuario");
+var contrasena = document.getElementsById("campoConstrasena");
+var error = document.getElementsById("error");
+error.style.color = "red"; 
+
+function validar_usuario() {
+    console.log("Enviando formulario...");
+
+    var mensajesError = [];
+
+    if (nombreUsuario === null || nombreUsuario.value === ""){
+        mensajesError.push("Ingresa tu nombre");
+    }
+
+    if (contrasena === null || contrasena.value === ""){
+        mensajesError.push("Ingresa tu contraseña");
+    }
+
+    error.innerHTML = mensajesError.join(", ");
+
+    return false; 
+}
